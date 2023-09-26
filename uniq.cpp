6f76a53d28df3bdc175738c32606a5564285b7d8
E Perkins <80359562+goldenpilipala@users.noreply.github.com>
2023-09-26 10:20:24 -0600

    Add files via upload

---
+++ uniq.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+void addToVector(vector<string>& myVector, set<string>& mySet, string line){
+    if(mySet.insert(line).second){ //If it is unique and inserts
+        myVector.push_back(line);
+        cout << line << endl;
+    }
+}
+
+//TODO: fix bug, last element always prints, even though it may be repeated
+
+int main(int argc, char const* argv[]) {
+    string textFile = "null";
+    string cinLine;
+    string fileLine;
+
+    vector<string> inputOrder; //To maintain insertion order
+    set<string> mySet; //For uniqueness
+
+    if(argc > 1){
+        textFile = argv[1];
+
+        ifstream ifs(textFile);
+        while(getline(ifs, fileLine)){
+            addToVector(inputOrder, mySet, fileLine);
+        }
+    }
+    else{
+        while(getline(cin, cinLine)){
+            addToVector(inputOrder, mySet, cinLine);
+        }
+    }
+
+    cout << " -------------------------- " << endl;
+
+    //Print values
+    for(const string& str : inputOrder){
+        cout << str << endl;
+    }
+
+    return 0;
+}
