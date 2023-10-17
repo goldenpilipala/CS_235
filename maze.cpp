e30b809d5e6ba02c588866f208fc44d1c88f0638
E Perkins <80359562+goldenpilipala@users.noreply.github.com>
2023-10-16 23:25:38 -0600

    Add files via upload

--- maze.cpp
+++ maze.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 //Global variable
 ofstream ofs;
+vector<string> solution;
 
 bool solveMaze(Grid maze, int r, int c, int l){
     int maxR = maze.height()-1;
@@ -25,13 +26,13 @@ bool solveMaze(Grid maze, int r, int c, int l){
     if(maze.at(r,c,l) == 2)
         return false; //I have been here before
 
+    if((r == maxR) && (c == maxC) && (l == maxL)){
+        solution.push_back(to_string(r) + " " + to_string(c) + " " + to_string(l));
+        return true; //Reached the end of the maze;
+    }
+
     //Mark cell as visited
     maze.at(r,c,l) = 2;
-    ofs << r << " " << c << " " << l << endl;
-
-    //Last base case
-    if((r == maxR) && (c == maxC) && (l == maxL))
-        return true; //Reached the end of the maze;
 
     //Recursion
     if  (solveMaze(maze, r+1, c, l) ||
@@ -40,11 +41,9 @@ bool solveMaze(Grid maze, int r, int c, int l){
         solveMaze(maze, r, c-1, l) ||
         solveMaze(maze, r, c, l+1) ||
         solveMaze(maze, r, c, l-1)){
+        solution.push_back(to_string(r) + " " + to_string(c) + " " + to_string(l));
         return true;
     }
-
-    // If no path is found, mark the cell as unvisited again
-    maze.at(r, c, l) = 1;
 
     return false;
 }
@@ -74,12 +73,15 @@ int main(int argc, char* argv[]) {
     int r=0, c=0, l=0;
     ofs.open(outputFile);
 
-    ofs << "SOLUTION" << endl;
-
     if(!solveMaze(maze, r, c, l)){
-        ofs.close();
-        ofs.open(outputFile);
         ofs << "NO SOLUTION";
+    }
+    else{
+        ofs << "SOLUTION" << endl;
+        for(auto itr = solution.rbegin(); itr != solution.rend(); itr++){
+            string& cell = *itr;
+            ofs << cell << endl;
+        }
     }
 
     //cout << maze.height() << " " << maze.width() << " " << maze.depth() << endl;
